f303decd5403d05166c896cc6fb0691d4b2a7323
Nercar <1042815796@qq.com>
2025-05-27 18:20:09 +0800

    ch

---
+++ src/scripts/clip_image.py
@@ -0,0 +1,20 @@
+# 裁决图像
+from tqdm import tqdm
+from pathlib import Path
+from PIL import Image
+from_folder = Path(fr"D:\NgDataSave\相机采集保存\join")
+out_folder = from_folder.parent/"crop____"
+out_folder.mkdir(exist_ok=True, parents=True)
+for f_ in tqdm(from_folder.glob(fr"*\*\*.jpg")):
+    image = Image.open(f_)
+    w,h = image.size
+
+
+    if "L1_g0_2345" in str(f_):
+        x_index = 0
+        index=0
+        for i in [1024, 1024],[1117, 1024],[1117, 1024],[1024, 1024]:
+            index+=1
+            c_image = image.crop((x_index,0,x_index+i[0],i[1]))
+            x_index+=i[0]
+            c_image.save(out_folder/(str(index)+f_.name))
