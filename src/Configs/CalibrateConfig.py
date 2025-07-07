2d60e16d923249c888e81ef1bbb3166de02bc6eb
Administrator <1042815796@qq.com>
2025-07-07 10:50:56 +0800

    ch

---
+++ src/Configs/CalibrateConfig.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+import cv2
+
+def split_image_horizontally(img, num_parts = 4):
+    # 获取图像尺寸
+    height, width = img.shape[:2]
+    # 计算每部分的高度
+    part_width = width // num_parts
+    # 存储分割后的图像
+    parts = []
+    for i in range(num_parts):
+        # 计算每部分的起始和结束y坐标
+        start_x = i * part_width
+        end_x = (i + 1) * part_width if i < num_parts - 1 else width
+        # 提取图像部分
+        part = img[:,start_x :end_x]
+        parts.append(part)
+    return parts
+
+class CalibrateConfig:
+    def __init__(self, image_list):
+        self.image_list = image_list
+        self.image_ = np.hstack(image_list)
+        self.mask_image=None
+
+    @property
+    def image(self):
+        return self.image_
+
+
+
+    @property
+    def sub_images(self):
+        image_list = self.image_list
+        if len(self.image_list) == 1:
+            image_list = split_image_horizontally(self.image_list[0])
+        print(f"sub_images {[i.shape for i in image_list]}")
+        return image_list
--- src/Configs/DebugFolderConfig.py
+++ src/Configs/DebugFolderConfig.py
@@ -3,13 +3,25 @@ from pathlib import Path
 
 
 class DebugFolderConfig:
-    if socket.gethostname() == 'DESKTOP-94ADH1G':
+    if socket.gethostname() == 'DESKTOP-3VCH6DO':
         # For coolbed
-        DATA_FOLDER = Path(r'/home/coolbed/steelSeg/datas')
-        MODEL_FOLDER = Path(r'/home/coolbed/steelSeg/models')
+        DATA_FOLDER = Path(r'I:\data_tool\PreData\ng\seg0611')
 
+        FOLDER_L1_g0_2345 = DATA_FOLDER / "L1_g0_2345"
+        FOLDER_L1_g1_6 = DATA_FOLDER / "L1_g1_6"
+        FOLDER_L1_g2_1 = DATA_FOLDER / "L1_g2_1"
 
+        FOLDER_L2_g0_2345 = DATA_FOLDER / "L2_g0_2345"
+        FOLDER_L2_g1_6 = DATA_FOLDER / "L2_g1_6"
+        FOLDER_L2_g2_1 = DATA_FOLDER / "L2_g2_1"
+        FOLDER_MAP = {
+            "L1_g0_2345": FOLDER_L1_g0_2345,
+            "L1_g1_6": FOLDER_L1_g1_6,
+            "L1_g2_1": FOLDER_L1_g2_1,
+            "L2_g0_2345": FOLDER_L2_g0_2345,
+            "L2_g1_6": FOLDER_L2_g1_6,
+            "L2_g2_1": FOLDER_L2_g2_1
+        }
 if __name__ == "__main__":
     print(socket.gethostname())
-    print(DebugFolderConfig.DATA_FOLDER)
-    print(DebugFolderConfig.MODEL_FOLDER)
+    print(DebugFolderConfig.DATA_FOLDER.exists())
